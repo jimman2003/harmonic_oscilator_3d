08a4d13b1de1f4dfedfc91572801d33cbd3d9228
jimman2003 <58356745+jimman2003@users.noreply.github.com>
2024-04-13 10:41:02 +0300

    add orbit controls

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import * as THREE from 'three';
-import { VelocityShader } from 'three/examples/jsm/Addons.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
+const controls = new OrbitControls( camera, renderer.domElement );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 const geometry = new THREE.BoxGeometry( 1, 1, 1);
@@ -15,10 +16,11 @@ scene.add( cube );
 camera.position.z = 5;
 function physics(force,mass) {
     acceleration=force/mass
+    
 }
 function animate() {
 	requestAnimationFrame( animate );
-    
+    controls.update();
 	renderer.render( scene, camera );
 }
 animate()
