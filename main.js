81784dcfda0a7e60aa5ef88e5ae05d00131efff6
jimman2003 <58356745+jimman2003@users.noreply.github.com>
2024-04-13 10:38:29 +0300

    Create basic scene

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 import * as THREE from 'three';
+import { VelocityShader } from 'three/examples/jsm/Addons.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+const geometry = new THREE.BoxGeometry( 1, 1, 1);
+const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+function physics(force,mass) {
+    acceleration=force/mass
+}
+function animate() {
+	requestAnimationFrame( animate );
+    
+	renderer.render( scene, camera );
+}
+animate()
