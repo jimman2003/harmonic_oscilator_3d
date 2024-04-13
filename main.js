449f83c8c57de3549712a1c5b6f16802191d5dfd
jimman2003 <58356745+jimman2003@users.noreply.github.com>
2024-04-13 10:58:45 +0300

    basic physics kinda working

--- main.js
+++ main.js
@@ -2,25 +2,33 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
-const controls = new OrbitControls( camera, renderer.domElement );
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-const geometry = new THREE.BoxGeometry( 1, 1, 1);
-const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+const controls = new OrbitControls(camera, renderer.domElement);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 camera.position.z = 5;
-function physics(force,mass) {
-    acceleration=force/mass
-    
+function physics(force, mass, dt) {
+    let acceleration = force / mass
+    let velocity = acceleration * dt
+    let displacement = velocity * dt
+    return displacement
 }
-function animate() {
-	requestAnimationFrame( animate );
+let lastTime = 0
+function animate(currentTime) {
+    requestAnimationFrame(animate);
     controls.update();
-	renderer.render( scene, camera );
+    let dt = currentTime - lastTime / 1000
+    let displacement = physics(0.1, 10000, dt)
+    console.log(displacement)
+    cube.position.x = displacement;
+    renderer.render(scene, camera);
+    lastTime = currentTime
 }
 animate()
